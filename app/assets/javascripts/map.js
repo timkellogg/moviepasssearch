ecfdbb79b6e5b63e4ac069c4fa2ecde17b77a4b9
Tim Kellogg <tim.kellogg@gmail.com>
2017-08-18 11:50:44 -0700

    add mobile styling

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-  window.map = L.map('map').setView([51.505, -0.09], 13);
+  window.map = L.map('map').setView([34.104341, -118.291647], 10);
 
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1Ijoic3VwZXJ1c2VyMjQiLCJhIjoiY2ltOWVzMmNlMDNuaHR5a2l1dzc4bjFlNCJ9.Tf-CNc_9NEq8W0FGUhn4fw', {
       attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
